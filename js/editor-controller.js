411f0a551587f229bdd971cb249aaa169c15deb2
Shaham Tamir <shahamt@gmail.com>
2025-04-02 12:20:40 +0300

    Add support for multiple elements and text shadow

--- js/editor-controller.js
+++ js/editor-controller.js
@@ -1,22 +1,6 @@
 'use strict'
 
-let gCurrMeme =
-{
-    id: null,
-    selectedTempId: 5,
-    selectedLineIdx: 0,
-    elements: [
-        {
-            type: 'Text',
-            txt: 'I sometimes eat Falafel',
-            stickerId: null,
-            size: 20,
-            color: 'red',
-            // pos: {x,y},
-            // dimens: {width,height}
-        }
-    ]
-}
+
 
 let gElCanvas
 let gCtx
@@ -35,6 +19,11 @@ function initCanvas() {
 }
 
 
+
+// ======== rendering template and elements ========
+
+//render the template of current meme and initiates the rendering of the meme elements
+
 function renderMeme() {
     const tempId = gCurrMeme.selectedTempId;
     const temp = getTempById(tempId);
@@ -46,7 +35,64 @@ function renderMeme() {
         const width = gElCanvas.width;
         const height = gElCanvas.height;
         gCtx.drawImage(img, 0, 0, width, height);
+        renderElements()
     };
 
     img.src = tempURL;
+}
+
+// runing over the array of meme elements and initiate element rendering for each
+
+function renderElements() {
+    gCurrMeme.elements.forEach(element => renderElement(element))
+}
+
+//checking the element type and initiaties the suitable rendering function
+
+function renderElement(element) {
+    console.log("element.type: ", element.type)
+    switch (element.type) {
+        case 'text':
+            renderText(element)
+
+            break
+        case 'sticker':
+            renderSticker(element)
+            break
+    }
+}
+
+// renders text elements
+
+function renderText(element) {
+    //if there are no defined positions, set the text in the middle
+    const offsetX = element.pos.x ? element.pos.x : gElCanvas.width / 2
+    const offsetY = element.pos.y ? element.pos.y : gElCanvas.height / 2
+
+    const font = `${element.fontSize}px ${element.fontFamily}`
+    const text = element.txt
+
+    gCtx.lineWidth = element.lineWidth
+    gCtx.strokeStyle = element.strokeStyle
+    gCtx.fillStyle = element.fillStyle
+    gCtx.font = font
+    gCtx.textAlign = element.textAlign
+    gCtx.textBaseline = element.textBaseline
+
+    //shadow
+    gCtx.shadowColor = element.shadowColor
+    gCtx.shadowOffsetX = element.shadowOffsetX
+    gCtx.shadowOffsetY = element.shadowOffsetY
+    gCtx.shadowBlur = element.shadowBlur
+
+    //draw the text
+    gCtx.fillText(text, offsetX, offsetY)
+    gCtx.strokeText(text, offsetX, offsetY)
+
+}
+
+// renders sticker elements
+
+function renderSticker(element) {
+
 }
--- js/services/memes-services.js
+++ js/services/memes-services.js
@@ -4,4 +4,73 @@ const gSavedMemes = [
 
 ]
 
+let gCurrMeme =
+{
+    id: null,
+    selectedTempId: 5,
+    selectedLineIdx: 0,
+    elements: [
+        {
+            type: 'text',
 
+            // text params
+            txt: 'I sometimes eat Falafel',
+            fontSize: 40,
+            fontFamily: 'Arial',
+            lineWidth: 2,
+            strokeStyle: 'black',
+            fillStyle: 'red',
+            textAlign: 'center',
+            textBaseline: 'middle',
+
+            //sticker params
+            stickerId: null,
+
+            // shadow params
+            shadowColor: 'rgba(0, 0, 0, 0.6)',
+            shadowOffsetX: 4,
+            shadowOffsetY: 4,
+            shadowBlur: 3,
+
+            // position params
+            pos: { x: null, y: null },
+
+
+            // dimens: {width,height}
+        },
+
+        {
+            type: 'text',
+
+            // text params
+            txt: 'b;ah blah',
+            fontSize: 40,
+            fontFamily: 'Arial',
+            lineWidth: 2,
+            strokeStyle: 'black',
+            fillStyle: 'red',
+            textAlign: 'center',
+            textBaseline: 'middle',
+
+            //sticker params
+            stickerId: null,
+
+            // shadow params
+            shadowColor: 'rgba(0, 0, 0, 0.6)',
+            shadowOffsetX: 4,
+            shadowOffsetY: 4,
+            shadowBlur: 3,
+
+            // position params
+            pos: { x: 20, y: 50 },
+
+
+            // dimens: {width,height}
+        }
+
+    ]
+}
+
+function updateTextElement(){
+    
+}
